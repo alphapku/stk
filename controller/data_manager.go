c534daa94b66456e1b2296043fd8406db3df5ffb
Alpha <alpha.pku@gmail.com>
2022-10-08 11:08:29 +1100

    [controller] fix calculation after testings

--- controller/data_manager.go
+++ controller/data_manager.go
@@ -14,6 +14,15 @@ import (
 	log "StakeBackendGoTest/pkg/log"
 )
 
+const (
+	pctDecimalPlaces     = 2 // to show pct as xx.xx
+	satoshiDecimalPlaces = 8
+)
+
+var (
+	pctMultiplier = decimal.NewFromInt(100)
+)
+
 type DataManager struct {
 	// positions map[string]map[string]*resp.StakePosition
 
@@ -75,28 +84,38 @@ func (d *DataManager) onMarketPrices(prices []*stk.Price) {
 }
 
 func (d *DataManager) calcStakePosition(pos *stk.Position, prx *stk.Price) {
+	availForTrddingQty := pos.AvailableForTradingQty.StringFixed(satoshiDecimalPlaces)
+
 	mktPrice := calcMarketPrice(prx)
+	mktPriceStr := mktPrice.StringFixed(satoshiDecimalPlaces)
 	mktValue := pos.OpenQty.Mul(mktPrice)
+	mktValueStr := mktValue.StringFixed(satoshiDecimalPlaces)
 
 	priorValue := pos.OpenQty.Mul(prx.PriorClose)
+	priorClose := prx.PriorClose.StringFixed(satoshiDecimalPlaces)
+
 	dayPNL := mktValue.Sub(priorValue)
-	dayPNLPCT := dayPNL.Div(priorValue)
+	dayPNLStr := mktValue.Sub(priorValue).StringFixed(satoshiDecimalPlaces)
+	dayPNLPCT := dayPNL.Div(priorValue).Mul(pctMultiplier)
+	dayPNLPCTStr := dayPNLPCT.StringFixed(pctDecimalPlaces)
 
 	totalPNL := mktValue.Sub(pos.Cost)
-	totalPNLPCT := totalPNL.Sub(pos.Cost)
+	totalPNLStr := mktValue.Sub(pos.Cost).StringFixed(satoshiDecimalPlaces)
+	totalPNLPCT := totalPNL.Div(pos.Cost).Mul(pctMultiplier)
+	totalPNLPCTStr := totalPNLPCT.StringFixed(pctDecimalPlaces)
 
 	if stkPos, ok := d.positions[prx.Symbol]; ok {
-		// existed
+		// update the existed
 		// stkPos.OpenQty = pos.OpenQty.String()
-		// stkPos.AvailableForTradingQty = pos.AvailableForTradingQty.String()
+		stkPos.AvailableForTradingQty = availForTrddingQty
 		// stkPos.AveragePrice = pos.AveragePrice.String()
-		stkPos.MarketValue = pos.OpenQty.Mul(mktPrice).String()
-		stkPos.MarketPrice = mktPrice.String()
-		// stkPos.PriorClose = prx.PriorClose.String()
-		stkPos.DayProfitOrLoss = dayPNL.String()
-		stkPos.DayProfitOrLossPercent = dayPNLPCT.String()
-		stkPos.TotalProfitOrLoss = totalPNL.String()
-		stkPos.TotalProfitOrLossPercent = totalPNLPCT.String()
+		stkPos.MarketValue = mktValueStr
+		stkPos.MarketPrice = mktPriceStr
+		stkPos.PriorClose = priorClose
+		stkPos.DayProfitOrLoss = dayPNLStr
+		stkPos.DayProfitOrLossPercent = dayPNLPCTStr
+		stkPos.TotalProfitOrLoss = totalPNLStr
+		stkPos.TotalProfitOrLossPercent = totalPNLPCTStr
 		a, _ := json.Marshal(stkPos)
 		log.Logger.Debug("updated", zap.String("position", string(a)))
 	} else {
@@ -104,16 +123,16 @@ func (d *DataManager) calcStakePosition(pos *stk.Position, prx *stk.Price) {
 		d.positions[prx.Symbol] = &resp.StakePosition{
 			Symbol:                   prx.Symbol,
 			Name:                     pos.Name,
-			OpenQty:                  pos.OpenQty.String(),
-			AvailableForTradingQty:   pos.AvailableForTradingQty.String(),
-			AveragePrice:             pos.AveragePrice.String(),
-			MarketValue:              mktValue.String(),
-			MarketPrice:              mktPrice.String(),
-			PriorClose:               prx.PriorClose.String(),
-			DayProfitOrLoss:          dayPNL.String(),
-			DayProfitOrLossPercent:   dayPNLPCT.String(),
-			TotalProfitOrLoss:        totalPNL.String(),
-			TotalProfitOrLossPercent: totalPNLPCT.String(),
+			OpenQty:                  pos.OpenQty.StringFixed(satoshiDecimalPlaces),
+			AvailableForTradingQty:   availForTrddingQty,
+			AveragePrice:             pos.AveragePrice.StringFixed(satoshiDecimalPlaces),
+			MarketValue:              mktValueStr,
+			MarketPrice:              mktPriceStr,
+			PriorClose:               priorClose,
+			DayProfitOrLoss:          dayPNLStr,
+			DayProfitOrLossPercent:   dayPNLPCTStr,
+			TotalProfitOrLoss:        totalPNLStr,
+			TotalProfitOrLossPercent: totalPNLPCTStr,
 		}
 		a, _ := json.Marshal(d.positions[prx.Symbol])
 		log.Logger.Debug("created", zap.String("position", string(a)))
